699e40ab9be724aaabb24ce0e4e46e0cf047d1bc
ljw <103088618+LJW-C@users.noreply.github.com>
2025-11-22 10:23:15 +0800

    Replace Tent with OA for model evaluation

--- pmnet-test.py
+++ pmnet-test.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore")
 
 import pandas as pd
 import numpy as np
-from methods import Tent
+from methods import OA
 # torch
 import torch
 import torch.nn as nn
@@ -62,9 +62,9 @@ def RMSE(pred, target, metrics=None):
 
 def eval_model(model, test_loader, cfg=None, infer_img_path=''):
 
-    # 设置模型为评估模式
+
     model.eval()
-    TTA = getattr(methods, 'Tent').setup(model, args)
+    TTA = getattr(methods, 'OA').setup(model, args)
     # 初始化指标
     n_samples = 0
     total_MAE = 0
@@ -72,7 +72,7 @@ def eval_model(model, test_loader, cfg=None, infer_img_path=''):
     total_RMSE = 0
     total_NMSE = 0
 
-    pred_cnt = 1  # 从 1 开始计数
+    pred_cnt = 1  
 
     for inputs, targets in tqdm(test_loader):
         iteration = 0
@@ -80,87 +80,53 @@ def eval_model(model, test_loader, cfg=None, infer_img_path=''):
         inputs = inputs.cuda()
         targets = targets.cuda()
 
-        # 对输入应用噪声和随机黑块
-        # inputs = add_noise(inputs, noise_factor=0.05)
-        # inputs = add_white_patches(inputs, horizontal_size=(2, 5), vertical_size=(5, 2), num_patches=30)
-
-        # if infer_img_path != '':
-        #     for i in range(len(inputs)):
-        #         # 将输入张量转换为 NumPy 数组
-        #         input_img = inputs[i][0].cpu().detach().numpy()
-        #
-        #         # 重新缩放到 [0, 255]
-        #         input_img_rescaled = (input_img * 255).astype(np.uint8)
-        #
-        #         # 保存输入图像
-        #         img_name = os.path.join(infer_img_path, 'input_images', f'{pred_cnt}.png')
-        #         cv2.imwrite(img_name, input_img_rescaled)
-        #         pred_cnt += 1
-        #         if pred_cnt % 100 == 0:
-        #             print(f'{img_name} saved')
-
         with torch.no_grad():
             # preds = model(inputs)
             preds = TTA.forward(inputs,targets)
 
-            #
+
             preds = torch.clamp(preds, 0, 1)
             inference_images_path = os.path.join(os.path.split(args.model_to_eval)[-2], 'inference_images')
             input_images_path = os.path.join(os.path.split(args.model_to_eval)[-2], 'input_images')
             os.makedirs(inference_images_path, exist_ok=True)
             os.makedirs(input_images_path, exist_ok=True)
 
-
-            # 计算指标
             MAE = nn.L1Loss()(preds, targets).item()
             MSE = nn.MSELoss()(preds, targets).item()
             RMSE = torch.sqrt(((preds - targets) ** 2).mean()).item()
             NMSE = MSE / (nn.MSELoss()(targets, torch.zeros_like(targets)).item() + 1e-7)
 
-
-
-            # 累加指标
             total_MAE += MAE * inputs.size(0)
             total_MSE += MSE * inputs.size(0)
             total_RMSE += RMSE * inputs.size(0)
             total_NMSE += NMSE * inputs.size(0)
             n_samples += inputs.size(0)
 
-            # 保存预测和标签图像
             if infer_img_path != '':
                 for i in range(len(preds)):
-                    # 将预测和标签转换为 numpy 数组
                     pred_img = preds[i][0].cpu().detach().numpy()
                     target_img = targets[i][0].cpu().detach().numpy()
 
-                    # 重新缩放到 [0, 255]
                     pred_img_rescaled = (pred_img * 255).astype(np.uint8)
                     target_img_rescaled = (target_img * 255).astype(np.uint8)
 
-                    # 将预测和标签图像并排组合
                     combined_img = np.hstack((pred_img_rescaled, target_img_rescaled))
 
-                    # 保存组合后的图像
                     img_name = os.path.join(infer_img_path, 'inference_images', f'{pred_cnt}.png')
                     cv2.imwrite(img_name, combined_img)
                     pred_cnt += 1
                     if pred_cnt % 100 == 0:
                         print(f'{img_name} saved')
-
-    # 计算平均指标
     avg_MAE = total_MAE / n_samples
     avg_MSE = total_MSE / n_samples
     avg_RMSE = total_RMSE / n_samples
     avg_NMSE = total_NMSE / n_samples
 
-    # 打印总体指标
     print(f"Overall testing MAE: {avg_MAE}")
     print(f"Overall testing MSE: {avg_MSE}")
     print(f"Overall testing NMSE: {avg_NMSE}")
     print(f"Overall testing RMSE: {avg_RMSE}")
 
-
-    # 返回指标字典
     results = {
         'MAE': avg_MAE,
         'MSE': avg_MSE,
@@ -179,12 +145,11 @@ def load_config_module(module_name, class_name):
 
 if __name__ == "__main__":
 
-    # 手动设置参数，模拟命令行传递的方式
     class Args:
-        data_root = 'F:/doc/code/pmnet-main/data/USC/'  # 数据根目录
-        network = 'pmnet_v3'  # 网络类型，pmnet_v1 或 pmnet_v3
-        model_to_eval = 'checkpoints/USC_8H_8W.pt'  # 预训练模型路径
-        config = 'config_USC_pmnetV3_V2'  # 配置类名
+        data_root = 'F:/doc/code/pmnet-main/data/USC/'  
+        network = 'pmnet_v3'  
+        model_to_eval = 'checkpoints/USC_8H_8W.pt'  
+        config = 'config_USC_pmnetV3_V2'  
 
 
     args = Args()
@@ -230,8 +195,8 @@ if __name__ == "__main__":
         train_dataset, test_dataset = random_split(data_train, [train_size, test_size],
                                                    generator=torch.Generator(device='cpu')) # 改为 cpu
 
-        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=8) # 删掉 generator 参数即可
-        test_loader = DataLoader(test_dataset, batch_size=16, shuffle=True, num_workers=8) # 删掉 generator 参数即可
+        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=8)
+        test_loader = DataLoader(test_dataset, batch_size=16, shuffle=True, num_workers=8) 
         
 
     elif cfg.sampling == 'random':
